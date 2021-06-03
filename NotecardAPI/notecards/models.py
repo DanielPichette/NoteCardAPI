98525e99d6e163e88253d7b3092bb843a9bfabb5
DanielPichette <daniel.f.pichette@gmail.com>
2021-06-03 13:28:14 -0500

    created models in the models.py

--- NotecardAPI/notecards/models.py
+++ NotecardAPI/notecards/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Collection(models.Model):
+    name = models.CharField(max_length=50)
+    notecards = models.ForeignKey('NotecardAPI.Notecard', default=None, null=True, on_delete=models.CASCADE)
+
+    def __int__(self):
+        return self.name
+
+
+class Notecard(models.Model):
+    word = models.CharField(max_length=50)
+    definition = models.CharField(max_length=500)
+    collection = models.ForeignKey('NotecardAPI.Collection', default=None, null=True, on_delete=models.CASCADE)
+
+    def __int__(self):
+        return self.word
